6f3ce6090d335ef1cbea43a9aa15c1e5b8ef4190
iesplin <iesplin@users.noreply.github.com>
2019-10-22 15:16:52 +1000

    Temporary directory for downloading CA cert

--- files/download_ca_cert.py
+++ files/download_ca_cert.py
@@ -1,11 +1,12 @@
 import argparse
+import filecmp
 import os
 import pexpect.popen_spawn
 import shutil
 import signal
 import sys
+import tempfile
 import urllib.request
-import filecmp
 
 parser = argparse.ArgumentParser(description='Download the Burp Suite CA Certificate.')
 parser.add_argument('product', choices=['community','pro'], help='Burp Suite product type.')
@@ -22,23 +23,24 @@ exit_status = 0
 try:
     child = pexpect.popen_spawn.PopenSpawn('{java} -Djava.awt.headless=true -jar "{jar}"'.format(java=java_path, jar=jar_path), encoding='UTF-8')
     child.logfile = sys.stdout
-
     child.expect('Proxy service started')
-    tmp_cacert_file = cacert_file + ".tmp"
-    urllib.request.urlretrieve("http://localhost:8080/cert", tmp_cacert_file)
 
-    if os.path.exists(cacert_file):
-        if filecmp.cmp(cacert_file, tmp_cacert_file):
-            os.remove(tmp_cacert_file)
-            print("No changes to CA certificate.")
+    with tempfile.TemporaryDirectory() as tmp_dir_name:
+        tmp_cacert_file = os.path.join(tmp_dir_name, "ca.der")
+        urllib.request.urlretrieve("http://localhost:8080/cert", tmp_cacert_file)
+
+        if os.path.exists(cacert_file):
+            if filecmp.cmp(cacert_file, tmp_cacert_file):
+                print("No changes to CA certificate.")
+            else:
+                shutil.move(tmp_cacert_file, cacert_file)
+                os.chmod(cacert_file, 0o644)
+                print("CA certificate updated.")
         else:
             shutil.move(tmp_cacert_file, cacert_file)
             os.chmod(cacert_file, 0o644)
-            print("CA certificate updated. File saved as {cacert_file}".format(cacert_file=cacert_file))
-    else:
-        shutil.move(tmp_cacert_file, cacert_file)
-        os.chmod(cacert_file, 0o644)
-        print("CA certificate saved. File saved as {cacert_file}".format(cacert_file=cacert_file))
+            print("CA certificate saved.")
+
 except Exception as e: 
     print(e)
     exit_status = 1
